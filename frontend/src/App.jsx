a3a78591bc6d0ce7ad6a735e3ef94c5fb4dde7aa
humuraelvin <humura53@gmail.com>
2024-07-01 13:43:14 +0200

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <div>
-          <h1>SocialSphere</h1>
+        <div className='App'>
+         <div className="blur"></div>
+         <div className="blur"></div>
        </div>
     </>
   )
