91e62b00c3bf620d59922315a59ea52107a9bf60
humuraelvin <humura53@gmail.com>
2024-07-01 19:11:18 +0200

    changes made

---
+++ frontend/src/components/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from '../../assets/img/img1.png'
+import img2 from '../../assets/img/img2.png'
+import img3 from '../../assets/img/img3.png'
+import img4 from '../../assets/img/img4.png'
+
+export const Followers = [
+    { name: "Andrew Thomas", username: "AndrewThomas", img: img1 },
+    { name: "John Doe", username: "JohnDoe", img: img2 },
+    { name: "Peter", username: "Peter", img: img3 },
+    { name: "JudasIscariot", username: "JudasIscariot", img: img4 },
+];
