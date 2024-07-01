f0d08b0425cb22b3214e08aecb5ef8bb0c2e8898
humuraelvin <humura53@gmail.com>
2024-07-01 17:54:20 +0200

    changes made

--- frontend/src/components/LogoSearch/LogoSearch.jsx
+++ frontend/src/components/LogoSearch/LogoSearch.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Logo from '../../assets/img/Social-Media-freepik-1076-72dpi-removebg-preview.png'
+import './LogoSearch.css'
+import { UilSearch } from '@iconscout/react-unicons'
 
 const LogoSearch = () => {
   return (
     <div className='LogoSearch'>
-      LogoSearch
+      <img src={Logo} alt=""  className='logo'/>
+      <div className="Search">
+        <input type="text" placeholder='#Explore' />
+        <div className="s-icon">
+            <UilSearch />
+        </div>
+      </div>
     </div>
   )
 }
