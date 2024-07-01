654f15beb6063678c5aef80577951260662be1f9
humuraelvin <humura53@gmail.com>
2024-07-01 13:46:49 +0200

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
         <div className='App'>
-         <div className="blur"></div>
-         <div className="blur"></div>
+         <div className="blur" style={{top: '-18%', right: '0'}}></div>
+         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
        </div>
     </>
   )
