923372015ba73803d6d75d33940b6d3f20886fc4
humuraelvin <humura53@gmail.com>
2024-07-01 18:52:56 +0200

    changes made

---
+++ frontend/src/components/FollowersCard/FollowersCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FollowersCard.css'
+
+const FollowersCard = () => {
+  return (
+    <div className='FollowersCard'>
+      FollowersCard
+    </div>
+  )
+}
+
+export default FollowersCard
--- frontend/src/components/profileSide/ProfileSide.jsx
+++ frontend/src/components/profileSide/ProfileSide.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
 import './ProfileSide.css'
+import FollowersCard from '../FollowersCard/FollowersCard'
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
       <LogoSearch />
       <ProfileCard />
+      <FollowersCard />
     </div>
   )
 }
