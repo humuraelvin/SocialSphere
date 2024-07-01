d39ad209be8426341b8c68ea9a74455f9ca6d09b
humuraelvin <humura53@gmail.com>
2024-07-01 17:55:30 +0200

    changes made

---
+++ frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileCard = () => {
+  return (
+    <div className='ProfileCard'>
+      Profile Card
+    </div>
+  )
+}
+
+export default ProfileCard
--- frontend/src/components/profileSide/ProfileSide.jsx
+++ frontend/src/components/profileSide/ProfileSide.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
       <LogoSearch />
+      <ProfileCard />
     </div>
   )
 }
