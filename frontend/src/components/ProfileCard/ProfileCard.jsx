5e997f97ef5c3345d442a888dffd2a38a456bf4c
humuraelvin <humura53@gmail.com>
2024-07-01 18:24:12 +0200

    changes made

--- frontend/src/components/ProfileCard/ProfileCard.jsx
+++ frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Cover from '../../assets/img/cover.jpg'
+import Profile from '../../assets/img/profile.jpg'
+import './ProfileCard.css'
 
 const ProfileCard = () => {
   return (
     <div className='ProfileCard'>
-      Profile Card
+      <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+      </div>
+      <div className="ProfileName">
+        <span>Humura Elvin</span>
+        <span>Senior Backend developer</span>
+      </div>
     </div>
   )
 }
