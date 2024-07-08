c56b174ebec8fe987c2daadb7384401b60184f97
humuraelvin <humura53@gmail.com>
2024-07-08 22:31:48 +0200

    changes made

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+require('dotenv').config()
+import multer from 'multer'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
