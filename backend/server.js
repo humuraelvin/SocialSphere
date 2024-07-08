9e5929e785e198d84a3b40600fc562f702dd664a
humuraelvin <humura53@gmail.com>
2024-07-08 22:44:03 +0200

    changes made

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,27 @@ require('dotenv').config()
 import multer from 'multer'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import path from 'path'
+import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors())
+app.use("/assets", express.static(path.join(__dirname, '/public/assets')))
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "/public/assets");
+    },
+    filename: function (req, file, cb){
+        cb(null, file.originalname)
+    }
+})
