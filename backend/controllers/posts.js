21c68612c81065fb1ed21f0d7d76d8e1bcb0c97f
humuraelvin <humura53@gmail.com>
2024-07-09 18:49:32 +0200

    changes made

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -32,4 +32,39 @@ const createPost = async(req, res) => {
 
 }
 
-const getFeedPosts = async (req, res) => {}
+const getFeedPosts = async (req, res) => {
+    try {
+        
+        const post = await Post.find();
+
+        return res.status(200).json(post)
+
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error", error:error.message})
+    }
+}
+
+const getUserPosts = async (req, res) => {
+    try {
+        
+
+        const {userId} = req.params;
+
+        const post = await Post.find({ userId })
+
+        return res.status(200).json(post);
+
+    } catch (error) {
+        
+    }
+}
+
+const likePost = async (req, res) => {
+    try {
+        
+        
+
+    } catch (error) {
+        return res.status(500).json({ message:"Internal server error", error: error.message })
+    }
+}
