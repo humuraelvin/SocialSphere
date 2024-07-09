d8d18a3400396d8372bedc38f2ba421adb76da7a
humuraelvin <humura53@gmail.com>
2024-07-09 18:42:05 +0200

    changes made

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,1 +1,35 @@
 import Post from '../models/Post.js'
+import User from '../models/User.js';
+
+
+const createPost = async(req, res) => {
+
+    try {
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+
+        const newPost = new Post ({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: []
+        })
+
+        await newPost.save();
+
+        const post = await Post.find();
+
+        res.status(200).json(post)
+
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error", error: error.message})
+    }
+
+}
+
+const getFeedPosts = async (req, res) => {}
