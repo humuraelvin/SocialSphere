c7ded32b3faab8652715cafe430f22cf704d015b
humuraelvin <humura53@gmail.com>
2024-07-10 12:06:13 +0200

    changes made

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -36,7 +36,9 @@ const store = configureStore({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
     </Provider>
    
   </React.StrictMode>,
