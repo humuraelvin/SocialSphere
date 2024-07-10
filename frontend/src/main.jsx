3673bad5f7de5bbb6c4bc0b9eeeb1ce2379d6d9c
humuraelvin <humura53@gmail.com>
2024-07-10 11:54:05 +0200

    changes made

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import authReducer from './state/index.js'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
