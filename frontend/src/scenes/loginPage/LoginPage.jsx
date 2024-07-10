c8446deffd5466670139364fd8677dee027baffa
humuraelvin <humura53@gmail.com>
2024-07-10 19:52:55 +0200

    changes made

--- frontend/src/scenes/loginPage/LoginPage.jsx
+++ frontend/src/scenes/loginPage/LoginPage.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Box,Typography, useTheme, useMediaQuery } from '@mui/material'
+import React from 'react';
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
 import Form from './Form';
 
-
 const LoginPage = () => {
-
   const theme = useTheme();
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
-  return 
+  return (
     <Box>
       <Box width='100%' backgroundColor={theme.palette.background.alt} p='1rem 6%' textAlign='center'>
-      <Typography
+        <Typography
           fontWeight="bold"
           fontSize="clamp(1rem, 2rem, 2.25rem)"
           color="primary"
@@ -27,17 +25,14 @@ const LoginPage = () => {
         borderRadius='1.5rem'
         backgroundColor={theme.palette.background.alt}
       >
-
-          <Typography fontWeight='500' variant='h5' sx={{ mb:"1.5rem" }}>
-              Welcome to SocialSphere, Where Social Norms Take a Back Seat
-          </Typography>
+        <Typography fontWeight='500' variant='h5' sx={{ mb: "1.5rem" }}>
+          Welcome to SocialSphere, Where Social Norms Take a Back Seat
+        </Typography>
 
         <Form />
-
       </Box>
-
     </Box>
-  
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
