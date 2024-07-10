ef431bec65da8cdeda393d6d81e8751a68f7d15a
humuraelvin <humura53@gmail.com>
2024-07-10 15:45:58 +0200

    changes made

--- frontend/src/scenes/loginPage/LoginPage.jsx
+++ frontend/src/scenes/loginPage/LoginPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Box,Typography, useTheme, useMediaQuery } from '@mui/material'
+
 
 const LoginPage = () => {
-  return (
-    <div>
-      LoginPage
-    </div>
-  )
+
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+
+  return 
+    <Box>
+      <Box width='100%' backgroundColor={theme.palette.background.alt} p='1rem 6%' textAlign='center'>
+      <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+        >
+          <img src={Logo} alt="" className='logo' />SocialSphere
+        </Typography>
+      </Box>
+    </Box>
+  
 }
 
 export default LoginPage
