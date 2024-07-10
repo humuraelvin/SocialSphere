1494f39a2986e1d42a441e0a312aa93a7a4ec8c5
humuraelvin <humura53@gmail.com>
2024-07-10 21:48:33 +0200

    changes made

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -41,7 +41,7 @@ export const register = async(req, res) => {
 }
 
 
-const login = async (req, res) => {
+export const login = async (req, res) => {
     try {
         
         const { email, password } = req.body;
