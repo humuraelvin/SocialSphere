2cebc904184608cdb5ab6383e5b050cd44eeb58e
humuraelvin <humura53@gmail.com>
2024-07-10 21:56:59 +0200

    changes made

--- backend/server.js
+++ backend/server.js
@@ -19,36 +19,7 @@ import User from './models/User.js'
 import Post from './models/Post.js'
 import { users, posts } from './data/data.js'
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-// const app = express()
 
-// dotenv.config()
-
-// app.use(express.json())
-// app.use(helmet())
-// app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-// app.use(morgan("common"))
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors())
-// app.use("/assets", express.static(path.join(__dirname, '/public/assets')))
-
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb){
-//         cb(null, "/public/assets");
-//     },
-//     filename: function (req, file, cb){
-//         cb(null, file.originalname)
-//     }
-// })
-
-// const upload = multer({ storage })
-
-
-// app.post("/auth/register", upload.single("picture"), verifyToken, register)
-// app.post('/posts', verifyToken, upload.single("picture"), createPost);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
