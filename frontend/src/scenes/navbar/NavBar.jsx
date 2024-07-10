0243565634a90e51fe65fd59e60fb3ba19fb23ff
humuraelvin <humura53@gmail.com>
2024-07-10 14:13:26 +0200

    changes made

--- frontend/src/scenes/navbar/NavBar.jsx
+++ frontend/src/scenes/navbar/NavBar.jsx
@@ -142,6 +142,47 @@ const NavBar = () => {
                     <Close />
                   </IconButton>
               </Box>
+
+              <IconButton onClick={() => dispatch(setMode())}>
+            {
+              theme.palette.mode === "dark" ? (
+                <DarkMode sx={{ fontSize: '25px' }}/>
+              ):(
+                <LightMode sx={{ color:dark, fontSize: '25px'}} />
+              )
+            }
+          </IconButton>
+
+            <Message sx={{ fontSize: '25px'}} />
+            <Notifications sx={{ fontSize: '25px'}} />
+            <Help sx={{ fontSize: '25px'}} />
+            <FormControl variant='standard' value={fullName}>
+             <Select value={fullName} sx={{
+               backgroundColor: neutralLight,
+               width: '150px',
+               borderRadius: '0.25rem',
+               p: '0.25rem 1rem',
+               "& .MuiSvgIcon-root":{
+                pr: "0.25rem",
+                width: "3rem"
+               },
+               "&. MuiSelect-select:focus":{
+                backgroundColor: neutralLight
+               }
+             }}
+               input={<InputBase />}
+              >
+
+                <MenuItem value={fullName}>
+                 <Typography>fullName</Typography>
+                </MenuItem>
+
+                <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+
+              </Select>
+
+            </FormControl>
+
             </Box>
           )
         }
