5c87646b491d276de3cce4cc56e197f5b2045bfc
humuraelvin <humura53@gmail.com>
2024-07-10 12:16:56 +0200

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,16 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import HomePage from 'scenes/homePage/HomePage'
 import LoginPage from 'scenes/loginPage/LoginPage'
 import ProfilePage from 'scenes/profilePage/ProfilePage'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material'
+import { themeSettings } from 'theme.js'
 
 function App() {
+
+
+  
   
   return (
     <div className='app'>
