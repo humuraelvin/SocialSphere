e15e61fb446ca9ea6756848c42f9f99fd4be503b
humuraelvin <humura53@gmail.com>
2024-07-11 17:06:58 +0200

    changes made

--- frontend/src/scenes/widgets/PostsWidget.jsx
+++ frontend/src/scenes/widgets/PostsWidget.jsx
@@ -10,7 +10,17 @@ const PostsWidget = ({ userId, isProfile = false}) => {
     const token = useSelector((state) => state.token)
 
     const getPosts = async () => {
-        const response = await fetch("http://localhost:3001", {
+        const response = await fetch("http://localhost:3001/posts", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+        })
+
+        const data = await response.json()
+        dispatch(setPosts({ posts:data }))
+    }
+
+    const getUserPosts = async () => {
+        const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         })
