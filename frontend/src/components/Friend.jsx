5930a3822f556e21cbeddca051cf92246abd6ef1
humuraelvin <humura53@gmail.com>
2024-07-11 20:04:13 +0200

    changes made

--- frontend/src/components/Friend.jsx
+++ frontend/src/components/Friend.jsx
@@ -1,2 +1,3 @@
 import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
