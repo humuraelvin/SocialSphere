d506614855da85071a1bb5dbeaea6b0918b63f43
humuraelvin <humura53@gmail.com>
2024-07-11 11:23:28 +0200

    changes made

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -63,6 +63,8 @@ export const login = async (req, res) => {
 
         res.status(200).json({ token, user })
 
+        console.log(token);
+
     } catch (error) {
         res.status(500).json({message:"Internal server error", error:error.message})
     }
