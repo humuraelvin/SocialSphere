00bd0e2817712c19f171c530e8f1ea180491ff18
humuraelvin <humura53@gmail.com>
2024-07-11 12:50:58 +0200

    changes made

--- frontend/src/scenes/homePage/HomePage.jsx
+++ frontend/src/scenes/homePage/HomePage.jsx
@@ -4,6 +4,10 @@ import { useSelector } from 'react-redux'
 import NavBar from '../navbar/NavBar'
 
 const HomePage = () => {
+
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+  const [_id, picturePath] = useSelector((state) => state.user)
+
   return (
     <Box>
       <NavBar />
