ce27f100ece6d115043d00f4dd6bdf19c641fe06
humuraelvin <humura53@gmail.com>
2024-07-11 12:12:39 +0200

    changes made

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -17,5 +17,14 @@ import { useNavigate } from 'react-router-dom'
 
 
 const UserWidget = ({ userId, picturePath }) => {
-    
+    const [user, setUser] = useState(null)
+    const { pallete } = useTheme()
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token)
+    const dark = pallete.neutral.dark;
+    const medium = pallete.neutral.medium;
+    const main = pallete.neutral.main;
+
+    const getUser = 
+
 }
