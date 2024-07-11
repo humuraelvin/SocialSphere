5d88d7d6334f91601c25f52de7b38bcc203769e5
humuraelvin <humura53@gmail.com>
2024-07-11 14:14:33 +0200

    changes made

--- frontend/src/scenes/widgets/MyPostWidget.jsx
+++ frontend/src/scenes/widgets/MyPostWidget.jsx
@@ -150,8 +150,26 @@ const MyPostWidget = ({ picturePath }) => {
                 </FlexBetween>
 
                 {isNonMobileScreens ? (
-                    
-                )}
+                    <>
+                        <FlexBetween gap='0.25rem'>
+                            <GifBoxOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Clip</Typography>
+                        </FlexBetween>
+
+                        <FlexBetween gap='0.25rem'>
+                            <AttachFileOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Attachment</Typography>
+                        </FlexBetween>
+
+                        <FlexBetween gap='0.25rem'>
+                            <MicOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Audio</Typography>
+                        </FlexBetween>
+
+                    </>
+                ) : <FlexBetween gap='0.25rem'>
+                    <MoreHorizOutlined sx={{ color:mediumMain }}/>
+                    </FlexBetween>}
             </FlexBetween>
 
         </WidgetWrapper>
