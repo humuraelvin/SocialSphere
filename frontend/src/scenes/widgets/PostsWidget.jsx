3bd724e9464f4b896bcca4140e44b90c152274d5
humuraelvin <humura53@gmail.com>
2024-07-11 17:01:27 +0200

    changes made

--- frontend/src/scenes/widgets/PostsWidget.jsx
+++ frontend/src/scenes/widgets/PostsWidget.jsx
@@ -2,3 +2,13 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "../../state/index.js";
 import PostWidget from './PostWidget.jsx'
+
+const PostsWidget = ({ userId, isProfile = false}) => {
+
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.posts)
+    const token = useSelector((state) => state.token)
+
+}
+
+export default PostsWidget;
