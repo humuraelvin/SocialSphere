bee3ec4c06a6b66c96711eeb4b4797d2ea132f96
humuraelvin <humura53@gmail.com>
2024-07-12 00:26:05 +0200

    changes made

--- frontend/src/scenes/homePage/HomePage.jsx
+++ frontend/src/scenes/homePage/HomePage.jsx
@@ -39,6 +39,7 @@ const HomePage = () => {
         {isNonMobileScreens && (
           <Box flexBasis='26%'>
             <AdvertWidget />
+            <Box m='2rem 0'/>
           </Box>
         )}
 
