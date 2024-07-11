84711ba5c9850a4d6062955c1422145351fad6a9
humuraelvin <humura53@gmail.com>
2024-07-12 01:15:52 +0200

    changes made

--- frontend/src/scenes/profilePage/ProfilePage.jsx
+++ frontend/src/scenes/profilePage/ProfilePage.jsx
@@ -39,6 +39,37 @@ const ProfilePage = () => {
 
   return <Box>
     <NavBar />
+
+    <Box
+       width='100%'
+       padding='2rem 6%'
+       display={isNonMobileScreens ? "flex" : "block"}
+       gap='0.5rem'
+       justifyContent='space-between'
+      >
+
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+            <UserWidget userId={_id} picturePath={picturePath}/>
+        </Box>
+
+        <Box
+         flexBasis={isNonMobileScreens ? "42%" : undefined}
+         mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath}/>
+          <PostsWidget userId={_id} />
+        </Box>
+
+        {isNonMobileScreens && (
+          <Box flexBasis='26%'>
+            <AdvertWidget />
+            <Box m='2rem 0'/>
+            <FriendListWidget userId={ _id }/>
+          </Box>
+        )}
+
+      </Box>
+
   </Box>
 }
 
