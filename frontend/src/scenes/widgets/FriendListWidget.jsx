68c909c184d80f2a6b5f9aa10bad1511a6190469
humuraelvin <humura53@gmail.com>
2024-07-12 00:32:06 +0200

    changes made

--- frontend/src/scenes/widgets/FriendListWidget.jsx
+++ frontend/src/scenes/widgets/FriendListWidget.jsx
@@ -1,3 +1,6 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import Friend from "../../components/Friend";
 import WidgetWrapper from "../../components/WidgetWrapper";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "../../state/index.js";
