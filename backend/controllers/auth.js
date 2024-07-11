5aa03dbf8a222a1abf4b71dc60b92c4bc5cfeef5
humuraelvin <humura53@gmail.com>
2024-07-11 23:35:43 +0200

    changes made

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
             return res.status(400).json({message:"Invalid credentials"})
         }
 
-        const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
 
         delete user.password;
 
