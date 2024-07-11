9f24320f74eb725c475ff915492cac536052c394
humuraelvin <humura53@gmail.com>
2024-07-11 16:58:01 +0200

    changes made

---
+++ frontend/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,4 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state/index.js";
+import PostWidget from './PostWidget.jsx'
