9c37a24bd39b4b3ce2032aafdfec3c0f03ffb83f
humuraelvin <humura53@gmail.com>
2024-07-12 00:12:30 +0200

    changes made

--- frontend/src/scenes/widgets/AdvertWidget.jsx
+++ frontend/src/scenes/widgets/AdvertWidget.jsx
@@ -1,3 +1,12 @@
 import { Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
+
+
+const AdvertWidget = () => {
+
+    
+
+}
+
+export default AdvertWidget;
