e45647ed3ef70b95ce402dd1fcbb163b1ed774c5
humuraelvin <humura53@gmail.com>
2024-07-11 13:14:27 +0200

    changes made

---
+++ frontend/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,24 @@
+import {
+
+    EditOutlined,
+    DeleteOutline,
+    AttachFileOutlined,
+    GifBoxOutlined,
+    ImageOutlined,
+    MicOutlined,
+    MoreHorizOutlined
+
+} from '@mui/icons-material'
+
+import {
+     
+    Box,
+    Divider,
+    Typography,
+    InputBase,
+    useTheme,
+    Button,
+    IconButton,
+    useMediaQuery
+
+ } from '@mui/material'
