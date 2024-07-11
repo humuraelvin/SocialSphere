2beb8b512d5d9c1d86146e02d9ce6b0f2ce8876e
humuraelvin <humura53@gmail.com>
2024-07-11 13:08:42 +0200

    changes made

--- frontend/src/scenes/homePage/HomePage.jsx
+++ frontend/src/scenes/homePage/HomePage.jsx
@@ -3,6 +3,7 @@ import { Box, useMediaQuery } from '@mui/material'
 import { useSelector } from 'react-redux'
 import NavBar from '../navbar/NavBar'
 import UserWidget from '../../scenes/widgets/UserWidget'
+import MyPostWidget from '../../scenes/widgets/MyPostWidget'
 
 const HomePage = () => {
 
@@ -29,7 +30,7 @@ const HomePage = () => {
          flexBasis={isNonMobileScreens ? "42%" : undefined}
          mt={isNonMobileScreens ? undefined : "2rem"}
         >
-          
+          <MyPostWidget />
         </Box>
 
         {isNonMobileScreens && (
