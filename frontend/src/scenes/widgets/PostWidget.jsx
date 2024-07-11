a457da3bec9e2e5dcca139ab7755d71b4e6d8718
humuraelvin <humura53@gmail.com>
2024-07-11 19:57:22 +0200

    changes made

--- frontend/src/scenes/widgets/PostWidget.jsx
+++ frontend/src/scenes/widgets/PostWidget.jsx
@@ -8,4 +8,4 @@ import {
 } from '@mui/icons-material'
 import { Box, Divider, IconButton, Typography, useTheme } from '@mui/material'
 import FlexBetween from '../../components/FlexBetween'
-import Friend from 
+import Friend from '../../components/Friend'
