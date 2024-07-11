df22be0d8e2a3448cd6ac9afd9a1bfa706cb6aa9
humuraelvin <humura53@gmail.com>
2024-07-11 14:00:49 +0200

    changes made

--- frontend/src/scenes/widgets/MyPostWidget.jsx
+++ frontend/src/scenes/widgets/MyPostWidget.jsx
@@ -94,9 +94,32 @@ const MyPostWidget = ({ picturePath }) => {
                      mt='1rem'
                     >
 
-                        <Dropzone>
-                            
-                        </Dropzone>
+                <Dropzone
+                    acceptedFiles=".jpg,.jpeg,.png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1rem"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picture ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
 
                     </Box>
                 )
