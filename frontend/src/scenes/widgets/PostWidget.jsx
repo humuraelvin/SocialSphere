2b1dcf0d25e30f77f40cb0aaea797be644e47498
humuraelvin <humura53@gmail.com>
2024-07-11 20:45:45 +0200

    changes made

--- frontend/src/scenes/widgets/PostWidget.jsx
+++ frontend/src/scenes/widgets/PostWidget.jsx
@@ -9,3 +9,6 @@ import {
 import { Box, Divider, IconButton, Typography, useTheme } from '@mui/material'
 import FlexBetween from '../../components/FlexBetween'
 import Friend from '../../components/Friend'
+import WidgetWrapper from '../../components/WidgetWrapper'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
