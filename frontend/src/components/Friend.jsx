f4ade8ae72e80cc20032cc694fd34b1f8eb1d252
humuraelvin <humura53@gmail.com>
2024-07-11 20:19:15 +0200

    changes made

--- frontend/src/components/Friend.jsx
+++ frontend/src/components/Friend.jsx
@@ -4,7 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from '../state/index.js'
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
+import { useNavigate } from "react-router-dom";
 
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { _id } = useSelector((state) => state.user);
+    const token  = useSelector((state) => state.token)
+    const friends = useSelector((state) => state.user.friends)
+
+    const { palette } = useTheme();
+
 }
