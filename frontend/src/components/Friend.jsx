ee4d2593a1dca407d3ab09717aa3a79005b16e34
humuraelvin <humura53@gmail.com>
2024-07-11 20:12:23 +0200

    changes made

--- frontend/src/components/Friend.jsx
+++ frontend/src/components/Friend.jsx
@@ -2,3 +2,6 @@ import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from '../state/index.js'
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+
